a9dc5f0a7fd3a8631da1a656ef2731b9f00092f2
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-07-30 18:40:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args){
+		
+		System.out.println("\t\t=================================\n");
+		
+		System.out.println("\t\t=\tStudent Information     \t=\n");
+		System.out.println("\t\t=================================\n");
+		System.out.println("\t\t= Name\t\t  : Sunil        \t=\n");
+		System.out.println("\t\t= Reg No\t  : ITXXXXXXX     \t=\n");
+		System.out.println("\t\t= Address\t  : Malabe        \t=\n");
+		System.out.println("\t\t=================================\n");
+		
+	}
+}
